552afcd33d890d2798b52919c0b4c0d146b7d914
Richard Boulton <richard.boulton@digital.cabinet-office.gov.uk>
2015-01-22 08:52:16 +0000

    Add docstrings to all functions

--- make_ids.py
+++ make_ids.py
@@ -7,11 +7,33 @@ import sys
 
 
 def format_entities_as_list(entities):
+    """Format entities read from an iterator as lists.
+
+    :param entities: An iterator yielding entities as dicts:
+           eg {"terms": ["Fred"]}
+
+    Yield a sequence of entites formatted as lists containing string values.
+    Also allocates identifier numbers.  Sequences are formatted as json.  eg:
+
+        ["1", '["Fred"]']
+
+    The resulting sequence is ideal for conversion to CSV.
+
+    """
     for i, entity in enumerate(entities, 1):
         yield (unicode(i), json.dumps(entity["terms"]))
 
 
 def generate_entities(fobj):
+    """Generate entities by reading from a file object.
+
+    :param fobj: File object to read from.  Each line in the file should
+    represent an entity.
+
+    Yields a sequence of dicts representing entities, where the dicts will
+    contain at the least a "terms" object.
+
+    """
     termsets_seen = set()
     for line in fobj:
         entity = json.loads(line)
@@ -21,7 +43,15 @@ def generate_entities(fobj):
             yield entity
 
 
-def load_entities_from_file(infile, outfile):
+def convert_entities_from_file_to_csv(infile, outfile):
+    """Convert entities from a file to CSV format.
+
+    :param infile: The file name to read entities from.  Formatted as jsonlines
+    (http://jsonlines.org/) - one line per entity.
+
+    :param outfile: The file name to write entities to as CSV.
+
+    """
     if os.path.exists(outfile):
         raise RuntimeError("Output file %r already exists" % outfile)
     with open(infile) as in_fobj:
@@ -32,4 +62,4 @@ def load_entities_from_file(infile, outfile):
 
 
 if __name__ == '__main__':
-    load_entities_from_file(sys.argv[1], sys.argv[2])
+    convert_entities_from_file_to_csv(sys.argv[1], sys.argv[2])
